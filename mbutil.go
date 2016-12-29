dd880ce3f6a04e3766c2076d1e15b0ec964ce1cd
Rag Sagar <ragsagar@gmail.com>
2016-12-29 14:02:01 +0400

    Add metadata to the metadata table in mbtile.

--- mbutil.go
+++ mbutil.go
@@ -3,7 +3,9 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/satori/go.uuid"
 	"io/ioutil"
 	"log"
 	"math"
@@ -21,6 +23,11 @@ const RAD_TO_DEG = 180 / math.Pi
 const MAX_LATITUDE = 85.0511287798
 const DEFAULT_TILE_SIZE = 256
 const MAX_ZOOM_LEVEL = 17
+const PNG_IMAGE_FORMAT = "image/png"
+const JPG_IMAGE_FORMAT = "image/jpg"
+const JPG_EXTENSION = "jpg"
+const PNG_EXTENSION = "png"
+const MBTILE_VERSION = "1.2"
 
 var MAPTYPES = []string{"http://mt2.google.com/vt/lyrs=y&x={x}&y={y}&z={z}", "http://c.tile.openstreetmap.org/{z}/{x}/{y}.png", "http://a.tiles.mapbox.com/v4/rsagar.n724o8le/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicnNhZ2FyIiwiYSI6IjM5OWVlZTVlYzJiYjhmMzAyMGMwMDBiYzA4NjEzMWM3In0.gc0JW6Ddp0RD_yBaaPE1vg"}
 
@@ -32,68 +39,6 @@ type Tile struct {
 func (tile *Tile) flipped_y() int {
 	two_power_zoom := math.Pow(2.0, float64(tile.z))
 	return int(two_power_zoom) - 1 - tile.y
-}
-
-type Projection struct {
-	Bc, Cc, Ac             []float64
-	Zc                     [][]float64
-	levels                 []int
-	xmin, ymin, xmax, ymax float64
-}
-
-func NewProjection(xmin, ymin, xmax, ymax float64, zoomlevel, max_zoomlevel int) *Projection {
-	proj := Projection{xmin: xmin, ymin: ymin, xmax: xmax, ymax: ymax}
-	for i := zoomlevel; i <= max_zoomlevel; i++ {
-		proj.levels = append(proj.levels, i)
-	}
-
-	var e float64
-	var c = float64(DEFAULT_TILE_SIZE)
-	for i := 0; i <= max_zoomlevel; i++ {
-		e = c / 2.0
-		proj.Bc = append(proj.Bc, c/360.0)
-		proj.Cc = append(proj.Cc, c/(2.0*math.Pi))
-		proj.Zc = append(proj.Zc, []float64{e, e})
-		proj.Ac = append(proj.Ac, c)
-		c = c * 2
-	}
-	return &proj
-}
-
-func (proj *Projection) project_pixels(x, y float64, zoom int) []float64 {
-	d := proj.Zc[zoom]
-	e := Round(d[0] + x*proj.Bc[zoom])
-	f := minMax(math.Sin(DEG_TO_RAD*y), -0.9999, 0.9999)
-	g := Round(d[1] + 0.5*math.Log((1+f)/(1-f))*-proj.Cc[zoom])
-	return []float64{e, g}
-}
-
-func (proj *Projection) TileList() []Tile {
-	var tilelist []Tile
-
-	for _, zoom := range proj.levels {
-		two_power_zoom := math.Pow(2, float64(zoom))
-		px0 := proj.project_pixels(proj.xmin, proj.ymax, zoom) // left top
-		px1 := proj.project_pixels(proj.xmax, proj.ymin, zoom) // right bottom
-		xrangeStart := int(px0[0] / DEFAULT_TILE_SIZE)
-		xrangeEnd := int(px1[0] / DEFAULT_TILE_SIZE)
-		for x := xrangeStart; x <= xrangeEnd; x++ {
-			if x < 0 || float64(x) >= two_power_zoom {
-				continue
-			}
-			yrangeStart := int(px0[1] / DEFAULT_TILE_SIZE)
-			yrangeEnd := int(px1[1] / DEFAULT_TILE_SIZE)
-			for y := yrangeStart; y <= yrangeEnd; y++ {
-				if y < 0 || float64(y) >= two_power_zoom {
-					continue
-				}
-				// y = (int(two_power_zoom) - 1) - y
-				tilelist = append(tilelist, Tile{z: zoom, x: x, y: y})
-			}
-		}
-
-	}
-	return tilelist
 }
 
 func mbTileWorker(db *sql.DB, tilePipe chan Tile, outputPipe chan Tile) {
@@ -161,7 +106,7 @@ func prepareDatabase(filename string) (*sql.DB, error) {
 	return db, nil
 }
 
-func setupMBTileTables(db *sql.DB) error {
+func setupMBTileTables(db *sql.DB, proj *Projection) error {
 
 	_, err := db.Exec("create table if not exists tiles (zoom_level integer, tile_column integer, tile_row integer, tile_data blob);")
 	if err != nil {
@@ -181,6 +126,14 @@ func setupMBTileTables(db *sql.DB) error {
 	_, err = db.Exec("create unique index tile_index on tiles(zoom_level, tile_column, tile_row);")
 	if err != nil {
 		return err
+	}
+
+	// Load metadata.
+	for name, value := range proj.MetaDataItems() {
+		_, err := db.Exec("insert into metadata (name, value) values (?, ?)", name, value)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -251,7 +204,7 @@ func main() {
 	}
 	defer db.Close()
 
-	err = setupMBTileTables(db)
+	err = setupMBTileTables(db, proj)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -283,6 +236,132 @@ func main() {
 	}
 	log.Println("Generated ", filename)
 
+}
+
+type Projection struct {
+	Bc, Cc, Ac             []float64
+	Zc                     [][]float64
+	levels                 []int
+	xmin, ymin, xmax, ymax float64
+	metaData               MetaData
+}
+
+func NewProjection(xmin, ymin, xmax, ymax float64, zoomlevel, max_zoomlevel int) *Projection {
+	proj := Projection{xmin: xmin, ymin: ymin, xmax: xmax, ymax: ymax}
+	for i := zoomlevel; i <= max_zoomlevel; i++ {
+		proj.levels = append(proj.levels, i)
+	}
+
+	var e float64
+	var c = float64(DEFAULT_TILE_SIZE)
+	for i := 0; i <= max_zoomlevel; i++ {
+		e = c / 2.0
+		proj.Bc = append(proj.Bc, c/360.0)
+		proj.Cc = append(proj.Cc, c/(2.0*math.Pi))
+		proj.Zc = append(proj.Zc, []float64{e, e})
+		proj.Ac = append(proj.Ac, c)
+		c = c * 2
+	}
+	bounds := fmt.Sprintf("%f,%f,%f,%f", xmin, ymin, xmax, ymax)
+	proj.metaData = NewMetaData(PNG_IMAGE_FORMAT, zoomlevel, max_zoomlevel, bounds)
+	return &proj
+}
+
+func (proj *Projection) project_pixels(x, y float64, zoom int) []float64 {
+	d := proj.Zc[zoom]
+	e := Round(d[0] + x*proj.Bc[zoom])
+	f := minMax(math.Sin(DEG_TO_RAD*y), -0.9999, 0.9999)
+	g := Round(d[1] + 0.5*math.Log((1+f)/(1-f))*-proj.Cc[zoom])
+	return []float64{e, g}
+}
+
+func (proj *Projection) TileList() []Tile {
+	var tilelist []Tile
+
+	for _, zoom := range proj.levels {
+		two_power_zoom := math.Pow(2, float64(zoom))
+		px0 := proj.project_pixels(proj.xmin, proj.ymax, zoom) // left top
+		px1 := proj.project_pixels(proj.xmax, proj.ymin, zoom) // right bottom
+		xrangeStart := int(px0[0] / DEFAULT_TILE_SIZE)
+		xrangeEnd := int(px1[0] / DEFAULT_TILE_SIZE)
+		for x := xrangeStart; x <= xrangeEnd; x++ {
+			if x < 0 || float64(x) >= two_power_zoom {
+				continue
+			}
+			yrangeStart := int(px0[1] / DEFAULT_TILE_SIZE)
+			yrangeEnd := int(px1[1] / DEFAULT_TILE_SIZE)
+			for y := yrangeStart; y <= yrangeEnd; y++ {
+				if y < 0 || float64(y) >= two_power_zoom {
+					continue
+				}
+				// y = (int(two_power_zoom) - 1) - y
+				tilelist = append(tilelist, Tile{z: zoom, x: x, y: y})
+			}
+		}
+
+	}
+	return tilelist
+}
+
+func (proj *Projection) MetaDataItems() map[string]string {
+	return proj.metaData.Items()
+}
+
+type MetaData struct {
+	tileFormat  string
+	name        string
+	description string
+	minZoom     string
+	maxZoom     string
+	bounds      string
+	_type       string
+	version     string
+}
+
+func NewMetaData(tileFormat string, minZoom int, maxZoom int, bounds string) MetaData {
+	uid := uuid.NewV4()
+	metaData := MetaData{
+		tileFormat:  tileFormat,   // required
+		name:        uid.String(), // required
+		description: uid.String(), // required
+		minZoom:     strconv.Itoa(minZoom),
+		maxZoom:     strconv.Itoa(maxZoom),
+		bounds:      bounds,
+		version:     MBTILE_VERSION, // required
+		_type:       "overlay",      // required
+	}
+	return metaData
+}
+
+func (metaData MetaData) TileExtension() string {
+	if metaData.tileFormat == PNG_IMAGE_FORMAT {
+		return PNG_EXTENSION
+	} else if metaData.tileFormat == JPG_IMAGE_FORMAT {
+		return JPG_EXTENSION
+	}
+	return PNG_EXTENSION
+}
+
+func (metaData MetaData) TileFormat() string {
+	return metaData.tileFormat
+}
+
+func (metaData MetaData) Version() string {
+	return metaData.version
+}
+
+func (metaData MetaData) Items() map[string]string {
+	data := map[string]string{
+		"name":        metaData.name,
+		"description": metaData.description,
+		"format":      metaData.TileExtension(),
+		"version":     metaData.version,
+		"type":        metaData._type,
+		"bounds":      metaData.bounds,
+		"minzoom":     metaData.minZoom,
+		"maxzoom":     metaData.maxZoom,
+	}
+	return data
 }
 
 func minMax(a, b, c float64) float64 {
